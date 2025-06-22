000a4490e8a563cf1345d1ef89f9506ef4281c21
postikus <karpov.ignat@gmail.com>
2025-06-22 03:37:02 +0300

    Update script1 with commented code and config adjustments
    - Comment out Button6 click and screenshot functionality
    - Update config.yaml with latest parameters
    - Clean up script1 implementation

--- internal/scripts/script1/scipt1.go
+++ internal/scripts/script1/scipt1.go
@@ -68,7 +68,7 @@ var Run = func(port *serial.Port, c *config.Config) {
 
 	var checkAndClickScreenScroll = func(counter int) (int, int) {
 		img := captureScreenShot()
-		r, _, _, _ := imageInternal.GetPixelColor(img, 283, 342)
+		r, _, _, _ := imageInternal.GetPixelColor(img, 283, 332)
 		if r < 50 {
 			scripts.FastClick(port, c)
 		}
@@ -94,7 +94,7 @@ var Run = func(port *serial.Port, c *config.Config) {
 		var screenshots []image.Image
 		var smallScreenshots []image.Image
 
-		img := saveScreenShot()
+		img := captureScreenShot()
 		screenshots = append(screenshots, img)
 		scrollRPx, scrollGPx, scrollBPx, _ := imageInternal.GetPixelColor(img, 290, 15)
 		fmt.Printf("scrollRPx: %v %v %v\n", scrollRPx, scrollGPx, scrollBPx)
@@ -122,7 +122,7 @@ var Run = func(port *serial.Port, c *config.Config) {
 			finalImage, _ := imageInternal.CombineImages(screenshots, smallScreenshots)
 			combinedImg := imageInternal.CropOpacityPixel(finalImage)
 
-			fileCount, err := countFilesInDir("./imgs")
+			fileCount, _ := countFilesInDir("./imgs")
 			fileName := fmt.Sprintf("%s/screenshot_combined_%d.png", "./imgs", fileCount)
 			err = imageInternal.SaveCombinedImage(combinedImg, fileName)
 			if err != nil {
@@ -148,68 +148,68 @@ var Run = func(port *serial.Port, c *config.Config) {
 		// прокликиваем первую страницу
 		columns, _ := imageInternal.FindNonBlackPixelCoordinatesInColumn(img, marginX+58)
 		if len(columns) > 2 {
-			var ys = imageInternal.FindSegments(columns, 10)
+			var ys = imageInternal.FindSegments(columns, 8)
 
 			fmt.Printf("ys: %v\n", ys)
 
 			for _, y := range ys {
-				clickItem(config.Coordinates{Y: y + marginY + c.Screenshot.ItemList.Y, X: marginX + c.Screenshot.ItemList.X})
+				clickItem(config.Coordinates{Y: y + marginY - 5, X: marginX + 80})
 			}
 		}
 
 		// clickItem(config.Coordinates{X: marginX + c.Click.Item8.X, Y: marginY + c.Click.Item8.Y})
 	}
 
-	//берем в фокус и делаем скрин
-	scripts.ClickCoordinates(port, c, c.Click.Item1)
-	img = saveScreenShot()
-	clickEveryItemAnsScreenShot(img)
-
-	// // берем в фокус и делаем скрин
+	// //берем в фокус и делаем скрин
 	// scripts.ClickCoordinates(port, c, c.Click.Item1)
+	// img = saveScreenShot()
+	// clickEveryItemAnsScreenShot(img)
 
-	// cycles := 0
-	// for cycles < 50 {
-	// 	img, _ := screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 	clickEveryItemAnsScreenShot(img)
+	// берем в фокус и делаем скрин
+	scripts.ClickCoordinates(port, c, c.Click.Item1)
 
-	// 	scripts.ClickCoordinates(port, c, c.Click.Button2)
-	// 	img, _ = screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 	clickEveryItemAnsScreenShot(img)
+	cycles := 0
+	for cycles < 1 {
+		img := captureScreenShot()
+		clickEveryItemAnsScreenShot(img)
 
-	// 	scripts.ClickCoordinates(port, c, c.Click.Button3)
-	// 	img, _ = screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 	clickEveryItemAnsScreenShot(img)
+		scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Button2.X, Y: marginY + c.Click.Button2.Y})
+		img = captureScreenShot()
+		clickEveryItemAnsScreenShot(img)
 
-	// 	scripts.ClickCoordinates(port, c, c.Click.Button4)
-	// 	img, _ = screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 	clickEveryItemAnsScreenShot(img)
+		scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Button3.X, Y: marginY + c.Click.Button3.Y})
+		img = captureScreenShot()
+		clickEveryItemAnsScreenShot(img)
 
-	// 	scripts.ClickCoordinates(port, c, c.Click.Button5)
-	// 	img, _ = screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 	clickEveryItemAnsScreenShot(img)
+		scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Button4.X, Y: marginY + c.Click.Button4.Y})
+		img = captureScreenShot()
+		clickEveryItemAnsScreenShot(img)
 
-	// 	scripts.ClickCoordinates(port, c, c.Click.Button6)
-	// 	img, _ = screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 	clickEveryItemAnsScreenShot(img)
+		scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Button5.X, Y: marginY + c.Click.Button5.Y})
+		img = captureScreenShot()
+		clickEveryItemAnsScreenShot(img)
 
-	// 	ButtonsImg, _ := screenshot.CaptureScreenshot(c.Screenshot.ItemOffersListButtons)
-	// 	SixButtonPx, _, _, _ := imageInternal.GetPixelColor(ButtonsImg, 124, 14)
-	// 	maxSixButtonClicks := 0
+		scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Button6.X, Y: marginY + c.Click.Button6.Y})
+		img = captureScreenShot()
+		clickEveryItemAnsScreenShot(img)
 
-	// 	for SixButtonPx > 30 && maxSixButtonClicks < 50 {
-	// 		scripts.ClickCoordinates(port, c, c.Click.Button6)
-	// 		img, _ = screenshot.CaptureScreenshot(c.Screenshot.ItemList)
-	// 		clickEveryItemAnsScreenShot(img)
-	// 		ButtonsImg, _ := screenshot.CaptureScreenshot(c.Screenshot.ItemOffersListButtons)
-	// 		SixButtonPx, _, _, _ = imageInternal.GetPixelColor(ButtonsImg, 124, 14)
-	// 		maxSixButtonClicks += 1
-	// 	}
+		img = captureScreenShot()
+		SixButtonPx, _, _, _ := imageInternal.GetPixelColor(img, c.Click.Button6.X, 35)
+		maxSixButtonClicks := 0
 
-	// 	scripts.ClickCoordinates(port, c, c.Click.Back)
-	// 	scripts.ClickCoordinates(port, c, config.Coordinates{X: 35, Y: 107})
+		for SixButtonPx > 30 && maxSixButtonClicks < 50 {
+			scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Button6.X, Y: marginY + c.Click.Button6.Y})
+			img = captureScreenShot()
+			clickEveryItemAnsScreenShot(img)
+			img = captureScreenShot()
+			SixButtonPx, _, _, _ = imageInternal.GetPixelColor(img, c.Click.Button6.X, c.Click.Button6.Y)
+			maxSixButtonClicks += 1
+		}
 
-	// 	cycles += 1
-	// }
+		scripts.ClickCoordinates(port, c, config.Coordinates{X: marginX + c.Click.Back.X, Y: marginY + c.Click.Back.Y})
+		// scripts.ClickCoordinates(port, c, config.Coordinates{X: 35, Y: 107})
+
+		cycles += 1
+	}
 
 }
